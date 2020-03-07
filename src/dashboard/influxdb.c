ad0b50f9ee34345d02b98c2d20b5585041c5bf18
Yogesh Hegde <yogesh@iotiot.in>
2020-03-07 17:33:43 +0530

    dashboards: Add support to influxd.
    Uses libcurl to send data to influxdb server.
    Commit adds dependency to libcurl.
    
    Signed-off-by: Yogesh Hegde <yogesh@iotiot.in>

---
+++ src/dashboard/influxdb.c
@@ -0,0 +1,70 @@
+/** 
+ * @file        influxdb.c
+ * @brief       This file contains code for sending data to InfluxDB
+ *              using libcurl.
+ *
+ */
+
+/*
+ *#####################################################################
+ *  Initialization block
+ *  ---------------------
+ *#####################################################################
+ */
+
+/* --- Standard Includes --- */
+#include <stdarg.h>
+#include <string.h>
+#include <curl/curl.h>
+
+/* --- Project Includes --- */
+#include "influxdb.h"
+
+/*
+ *#####################################################################
+ *  Process block
+ *  -------------
+ *#####################################################################
+ */
+
+/**
+ * @brief Send Data to Influx DB
+ * 
+ */
+int8_t send_to_influxdb(struct InfluxdbSettings set, const char *fmt, ...)
+{
+        int8_t rc = -1;
+        va_list ap;
+        char payload [MAX_PAYLOAD_SIZE] ;
+
+        va_start(ap, fmt);
+        vsnprintf(payload, sizeof(payload), fmt, ap);
+        va_end(ap);
+
+	CURL *curl;
+	CURLcode res;
+
+        char influxdb_url[MAX_CURL_URL_PATH];
+        
+        snprintf(influxdb_url,
+                 sizeof(influxdb_url),
+                 "%s%s%s",
+                 set.dbURL,
+                 "/write?db=",
+                 set.dbName);
+	
+	curl = curl_easy_init();
+	if(curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, influxdb_url);
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload);
+		res = curl_easy_perform(curl);
+
+		if(res != CURLE_OK) {
+                        fprintf(stderr, "curl_easy_perform failed: %s\n", curl_easy_strerror(res));
+                }
+                curl_easy_cleanup(curl);
+                rc = 0;
+	}
+	curl_global_cleanup();
+	return rc;
+}
